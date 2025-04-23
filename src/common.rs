4bcd0153f58c41406fa57d9532e25111b7f99ca9
dingxlaowu <32857050+dingxlaowu@users.noreply.github.com>
2025-04-23 09:44:10 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -944,7 +944,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rustdesk.shantin.net:21114".to_owned()
+    "youdomain:port".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
